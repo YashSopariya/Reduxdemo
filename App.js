d3d5cf9764c27dc6474653805071622f45a8ce41
Yashsopariya1308 <39852985+Yashsopariya1308@users.noreply.github.com>
2019-04-29 12:56:36 +0530

    Redux with React native

---
+++ App.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList
+} from "react-native";
+import ListItem from "./components/ListItem";
+import { connect } from "react-redux";
+import { addPlace } from "./actions/place";
+
+class App extends Component {
+  state = {
+    placeName: "",
+    places: []
+  };
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    }
+    this.props.add(this.state.placeName);
+    this.setState({ placeName: "" });
+  };
+  placeNameChangeHandler = value => {
+    this.setState({
+      placeName: value
+    });
+  };
+
+  placesOutput = () => {
+    return (
+      <FlatList
+        style={styles.listContainer}
+        data={this.props.places}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={info => <ListItem placeName={info.item.value} />}
+      />
+    );
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Search Places"
+            style={styles.placeInput}
+            value={this.state.placeName}
+            autoFocus={true}
+            onChangeText={this.placeNameChangeHandler}
+          />
+          <Button
+            title="Add"
+            style={styles.placeButton}
+            onPress={this.placeSubmitHandler}
+          />
+        </View>
+        <View style={styles.listContainer}>{this.placesOutput()}</View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 30,
+    justifyContent: "flex-start",
+    alignItems: "center"
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "100%"
+  },
+  placeInput: {
+    width: "60%",
+    height: 40,
+    borderColor: "#000000",
+    borderWidth: 1
+  },
+  placeButton: {
+    width: "40%",
+    alignItems: "center",
+    paddingLeft: 10
+  },
+  listContainer: {
+    paddingTop:10,
+    width: "100%"
+  }
+});
+const mapStateToProps = state => {
+  return {
+    places: state.places.places
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    add: name => {
+      dispatch(addPlace(name));
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
